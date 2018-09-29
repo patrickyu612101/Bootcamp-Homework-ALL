e81344bc220716933c931b86eaaa5ba16a7609ea
Unknown <patrickyu612101@gmail.com>
2018-09-29 11:41:29 -0500

    done

--- Homework 10/StarterCode/static/js/app.js
+++ Homework 10/StarterCode/static/js/app.js
@@ -24,12 +24,29 @@ var inputValue = inputElement.property("value");
 
 var filteredData = tableData.filter(tdate => tdate.datetime === inputValue);
 
-console.log(filteredData);
+console.log("file",filteredData);
 // console.log(tableData);
 
-// var userdate= filteredData.map(time=>time.datetime);
-// console.log(userdate);
+// tbody.parentNode.removeChild();
+var tremove =d3.select("tbody");
+while (tremove.firstChild) {
+  tremove.removeChild(tremove.firstChild);
+}
 
+
+var userdate= filteredData.map(time=>time.datetime);
+console.log(userdate); 
+//new table
+tbody.html(" ");
+filteredData.forEach((filteredData) => {
+  var row = tbody.append("tr");
+  
+  Object.entries(filteredData).forEach(([key, value]) => {
+    var cell = tbody.append("td");
+    cell.text(value);
+  });
+});
+//
 
 });
 
